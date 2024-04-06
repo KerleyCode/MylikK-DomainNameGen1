c8c3687818233bd67ef4b400df7c87a5732fc05d
My'lik Kerley <159506912+KerleyCode@users.noreply.github.com>
2024-04-06 00:44:36 +0000

    final DomainGen

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "her", "their", "his"];
+  let adjective = ["great", "big", "happy", "brave"];
+  let noun = ["jogger", "raccoon", "chair"];
+  let www = ["www.", "wwww."];
+  let url = [".com", ".net", ".io", ".dev"];
+
+  console.log("Regular for loop:");
+  for (let wwwIndex = 0; wwwIndex < www.length; ++wwwIndex) {
+    for (let pronounIndex = 0; pronounIndex < pronoun.length; ++pronounIndex) {
+      for (let adjIndex = 0; adjIndex < adjective.length; ++adjIndex) {
+        for (let nounIndex = 0; nounIndex < noun.length; ++nounIndex) {
+          for (let urlIndex = 0; urlIndex < url.length; ++urlIndex) {
+            console.log(
+              `${www[wwwIndex]}${pronoun[pronounIndex]}${adjective[adjIndex]}${noun[nounIndex]}${url[urlIndex]}`
+            );
+          }
+        }
+      }
+    }
+  }
 };
